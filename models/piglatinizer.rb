cdc8bca3c398d91345596d81037a45a2c32dc6ff
David Markel <davidgmarkel@gmail.com>
2018-06-01 23:08:43 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,7 +2,26 @@ class PigLatinizer
 
   def initialize(word)
     @word = word
-  end 
+  end
+
+  def is_vowel(letter)
+    case letter.downcase
+    when "a"
+      true
+    when "e"
+      true
+    when "i"
+      true
+    when "o"
+      true
+    when "u"
+      true
+    else
+      false
+    end
+  end
+
+
 
   def piglatinize(word)
     if word[0].scan(/[aeiou]/) != []
