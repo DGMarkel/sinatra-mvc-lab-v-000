592a13c7c439f234c3633eeac6e36e65268dae14
David Markel <davidgmarkel@gmail.com>
2018-06-02 00:05:11 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,7 +14,7 @@ class PigLatinizer
       @word += "way"
     else
       array = @word.split /([aeiou].*)/
-      (array[1] + array[0]).join("ay")
+      "#{array[1] + array[0])}ay"
     end
   end
 
