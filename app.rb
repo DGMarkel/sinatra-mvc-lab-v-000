7a8e9bb7345d08041aca0c8589747a2511718342
David Markel <davidgmarkel@gmail.com>
2018-06-02 00:41:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,13 +2,13 @@ require_relative 'config/environment'
 require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
-  
+
   get '/' do
     erb :user_input
   end
 
   post '/' do
-    @words = PigLatinizer.new(params[:user_text])
+    @words = PigLatinizer.new(params[:user_phrase])
     erb :results
   end
 
